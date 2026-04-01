228c407c5b0af94b06c145cd1a235858f98bb891
wjwen <wjwen@wooduan.com>
2026-04-01 18:30:03 +0800

    feat: add NativeBridge to expose Vulkan RHI unit and rendering tests to C# test runner

--- Src/NativeBridge.cpp
+++ Src/NativeBridge.cpp
@@ -11,6 +11,16 @@
 #include "../RHI/Unit/RHIMultiQueueNativeTest.h"
 #include "../RHI/Unit/RHIMultiQueueResourceDisposalTest.h"
 
+// Rendering Tests
+#include "../RHI/Rendering/RHIBasicRenderingTest.h"
+#include "../RHI/Rendering/RHIGPUParticleTest.h"
+#include "../RHI/Rendering/RHIGeometryShaderTest.h"
+#include "../RHI/Rendering/RHIMeshShaderTest.h"
+#include "../RHI/Rendering/RHIMultiDrawIndirectTest.h"
+#include "../RHI/Rendering/RHIRayTracingTest.h"
+#include "../RHI/Rendering/RHITessellationShaderTest.h"
+#include "../RHI/Rendering/RHIVRSShadingRateTest.h"
+
 #include <iostream>
 
 #ifdef _WIN32
@@ -41,18 +51,40 @@ extern "C"
         TestRunner::RegisterTest<RHIMultiQueueNativeTest>();
         TestRunner::RegisterTest<RHIMultiQueueResourceDisposalTest>();
 
+        // Register Rendering Tests
+        TestRunner::RegisterTest<RHIBasicRenderingTest>();
+        TestRunner::RegisterTest<RHIGPUParticleTest>();
+        TestRunner::RegisterTest<RHIGeometryShaderTest>();
+        TestRunner::RegisterTest<RHIMeshShaderTest>();
+        TestRunner::RegisterTest<RHIMultiDrawIndirectTest>();
+        TestRunner::RegisterTest<RHIRayTracingTest>();
+        TestRunner::RegisterTest<RHITessellationShaderTest>();
+        TestRunner::RegisterTest<RHIVRSShadingRateTest>();
+
         // 2. Wrap them for the C# callback
         // This is a simple bridge that allows the C# TestRunner to "see" the native tests
-        registerCallback("Vulkan.Native.SyncTest", []() {
-             TestRunner::RunTestByName("RHISyncTest");
-        });
-        
-        registerCallback("Vulkan.Native.MultiThreadedTest", []() {
-             TestRunner::RunTestByName("RHIMultiThreadedTest");
-        });
 
-        registerCallback("Vulkan.Native.MemoryAliasingTest", []() {
-             TestRunner::RunTestByName("RHIMemoryAliasingTest");
-        });
+        // --- Unit Tests ---
+        registerCallback("Vulkan.Native.SyncTest", []() { TestRunner::RunTestByName("RHISyncTest"); });
+        registerCallback("Vulkan.Native.BindlessTest", []() { TestRunner::RunTestByName("RHIBindlessTest"); });
+        registerCallback("Vulkan.Native.MultiThreadedTest", []() { TestRunner::RunTestByName("RHIMultiThreadedTest"); });
+        registerCallback("Vulkan.Native.BatchApiTest", []() { TestRunner::RunTestByName("RHIBatchApiTest"); });
+        registerCallback("Vulkan.Native.SecondaryCommandBufferTest", []() { TestRunner::RunTestByName("RHISecondaryCommandBufferTest"); });
+        registerCallback("Vulkan.Native.AsyncComputeTest", []() { TestRunner::RunTestByName("RHIAsyncComputeTest"); });
+        registerCallback("Vulkan.Native.DebugTest", []() { TestRunner::RunTestByName("RHIDebugTest"); });
+        registerCallback("Vulkan.Native.InspectorTest", []() { TestRunner::RunTestByName("RHIInspectorTest"); });
+        registerCallback("Vulkan.Native.MemoryAliasingTest", []() { TestRunner::RunTestByName("RHIMemoryAliasingTest"); });
+        registerCallback("Vulkan.Native.MultiQueueNativeTest", []() { TestRunner::RunTestByName("RHIMultiQueueNativeTest"); });
+        registerCallback("Vulkan.Native.MultiQueueResourceDisposalTest", []() { TestRunner::RunTestByName("RHIMultiQueueResourceDisposalTest"); });
+
+        // --- Rendering Tests ---
+        registerCallback("Vulkan.Native.BasicRenderingTest", []() { TestRunner::RunTestByName("RHIBasicRenderingTest"); });
+        registerCallback("Vulkan.Native.GPUParticleTest", []() { TestRunner::RunTestByName("RHIGPUParticleTest"); });
+        registerCallback("Vulkan.Native.GeometryShaderTest", []() { TestRunner::RunTestByName("RHIGeometryShaderTest"); });
+        registerCallback("Vulkan.Native.MeshShaderTest", []() { TestRunner::RunTestByName("RHIMeshShaderTest"); });
+        registerCallback("Vulkan.Native.MultiDrawIndirectTest", []() { TestRunner::RunTestByName("RHIMultiDrawIndirectTest"); });
+        registerCallback("Vulkan.Native.RayTracingTest", []() { TestRunner::RunTestByName("RHIRayTracingTest"); });
+        registerCallback("Vulkan.Native.TessellationShaderTest", []() { TestRunner::RunTestByName("RHITessellationShaderTest"); });
+        registerCallback("Vulkan.Native.VRSShadingRateTest", []() { TestRunner::RunTestByName("RHIVRSShadingRateTest"); });
     }
 }
